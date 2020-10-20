300daf20bd66fdb0018ea46cc90779c01353674b
Preveen Raj <preveenraj@surveysparrow.com>
2020-10-21 01:02:32 +0530

    aliasing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Heythere from "heythere";
 
 class App extends React.Component {
   render() {
     const { name } = this.props;
     return (
       <>
+        <Heythere />
         <h1>Hello {name}</h1>
       </>
     );
---
+++ src/components/heythere.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Heythere extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>yo man</h1>
+      </>
+    );
+  }
+}
+
+export default Heythere;
