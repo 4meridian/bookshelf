73eaddc7c68c488dead363b62528a4ac581114b1
preveenraj <97preveenraj@gmail.com>
2021-01-03 23:43:08 +0530

    added hydration data

--- server/routes/root.js
+++ server/routes/root.js
@@ -6,7 +6,7 @@ const assetJson = require("../../assets.json");
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const assetPrefix = config.env == "development" ? "http://localhost:5678/" : "/public/dist/";
-  res.render('index', { config: { bundlePath: assetPrefix + assetJson.main.js } });
+  res.render('index', { config: { data: { authentication: false }, bundlePath: assetPrefix + assetJson.main.js } });
 });
 
 module.exports = router;
